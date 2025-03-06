0c84d77544d6494f974342999a638f2974b4efb9
Thuc Nguyen <ntthuc@gmail.com>
2025-03-06 13:28:55 -0400

    fundamental

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,13 @@
-function Book(props) {
-  const { img, title, author, children } = props
+const Book = (props) => {
+  const { img, title, author, number } = props
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
-      <button>click me</button>
+      <span className="number">{`# ${number + 1}`}</span>
     </article>
   )
 }
+
 export default Book
--- src/index.js
+++ src/index.js
@@ -2,77 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts for Curious Minds',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'Thuc Nguyen',
-    title: 'Java Programming',
-    img: './images/book-1.jpg',
-    id: 2,
-  },
-]
+import { books } from './books'
+import Book from './Book'
 
 function BookList() {
   return (
-    <section className='booklist'>
-      <EventExamples />
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className='booklist'>
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index}/>
       })}
     </section>
-  )
-}
-
-const EventExamples = () => {
-  const handleFormInput = () => {
-    console.log('handle form input.')
-  }
-  const handleButtonClick = () => {
-    alert('handle button click')
-  }
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-    console.log('form submitted')
-  }
-
-  return (
-    <section>
-      <form onSubmit={handleFormSubmit}>
-        <h2>Typical Form</h2>
-        <input
-          type='text'
-          name='example'
-          style={{ margin: '1rem 0' }}
-          onChange={handleFormInput}
-        ></input>
-        <div>
-          <button type='submit'>submit</button>
-        </div>
-        <button type='button' onClick={handleButtonClick}>
-          click me
-        </button>
-      </form>
-    </section>
-  )
-}
-
-function Book(props) {
-  const { img, title, author, children } = props
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      {children}
-      <button>click me</button>
-    </article>
+    </>
   )
 }
 
