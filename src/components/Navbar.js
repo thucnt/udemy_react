971aa34f65e319735cf2b45522aac7d10481858a
Thuc Nguyen <ntthuc@gmail.com>
2025-03-10 12:07:38 -0300

    resolve merge conflics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import logo from '../images/logo.svg'
-import { pageLinks, socialLinks } from '../data'
+import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
+  return (
+    <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="backroads" />
@@ -13,28 +13,36 @@ const Navbar = () => {
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-            {pageLinks.map((link) => {
-                return (
-                    <li key={link.id}>
-                        <a href={link.href} className="nav-link"> {link.text} </a>
-                    </li>          
-                )
-            })}
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {" "}
+                  {link.text}{" "}
+                </a>
+              </li>
+            );
+          })}
         </ul>
 
         <ul className="nav-icons">
-            {socialLinks.map((link) => {
-                return (
-                    <li key={link.id}>
-                    <a href={link.href} target="_blank" rel="noopener" className="nav-icon">
-                        <i className={link.icon}></i>
-                    </a>
-                </li>
-                )
-            })}
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
-    )
-}
-export default Navbar
+  );
+};
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-import { books } from './books'
-import Book from './Book'
-
-function BookList() {
-  return (
-    <>
-      <h1>Amazon Best Sellers</h1>
-      <section className='booklist'>
-      {books.map((book, index) => {
-        return <Book {...book} key={book.id} number={index}/>
-      })}
-    </section>
-    </>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BookList />)
->>>>>>> 0c84d77544d6494f974342999a638f2974b4efb9
